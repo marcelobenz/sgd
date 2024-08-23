2018463eb9c73cc737ecd9f26b62015e22ba37ea
Marcelo <glavecchia@cognisys.com.ar>
2024-08-23 12:03:06 -0300

    cambio logo en login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Tramites Online</title>
+    <title>{{ config('app.name', 'Sistema de Gestion Documental') }}</title>
     <!-- Incluye Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
