caf6bd187b5f298e6f0112286d2bbcf1455a0824
Marcelo <glavecchia@cognisys.com.ar>
2025-06-23 13:42:06 -0300

    arreglo ruta duplicada

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/documentos', [DocumentoController::class, 'index'])->name('documentos.index');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    #Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     // Ruta para mostrar el perfil del usuario
     Route::get('/perfil', [UserProfileController::class, 'show'])->name('profile.show');
