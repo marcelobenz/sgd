5d020773401416f7cf19b1f2bac5297eaec302ab
Ignacio Feder <ifeder@cognisys.com.ar>
2026-01-09 09:07:40 -0300

    mejora layout create y edit

--- resources/views/documentos/create.blade.php
+++ resources/views/documentos/create.blade.php
@@ -4,101 +4,112 @@
 @endsection
 
 @section('contenidoPrincipal')
-<div class="container" style="margin-top: 80px;">
-    <div class="w-100" style="background-color: #f8f9fa;">
-        <h2 class="text-center">Nuevo Documento</h2>
-    </div>
-
-    <form action="{{ route('documentos.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-
-        <div class="form-group">
-            <label for="titulo">Título</label>
-            <input type="text" name="titulo" id="titulo" class="form-control" required>
+    <div class="container" style="margin-top: 80px;">
+        <div class="w-100" style="background-color: #f8f9fa;">
+            <h2 class="text-center">Nuevo Documento</h2>
         </div>
 
-        <div class="form-group">
-            <label for="archivo">Archivo</label>
-            <input type="file" name="archivo" id="archivo" class="form-control" required
-            accept=".pdf, .doc, .docx, .xls, .xlsx, .ppt, .pptx">
-        </div>
+        <div class="container" style="margin-top: 80px;">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <form action="{{ route('documentos.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
 
-        <div class="form-group">
-            <label for="id_categoria">Categoría</label>
-            <select name="id_categoria" id="id_categoria" class="form-control" required>
-                @foreach($categorias as $categoria)
-                    <option value="{{ $categoria->id }}">{{ $categoria->nombre_categoria }}</option>
-                @endforeach
-            </select>
-        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="titulo">Título</label>
+                                    <input type="text" name="titulo" id="titulo" class="form-control" required>
+                                </div>
+                            </div>
 
-        <div class="form-group form-check mt-2">
-            <input type="checkbox" name="sin_aprobacion" id="sin_aprobacion" class="form-check-input">
-            <label for="sin_aprobacion" class="form-check-label">No requiere aprobación</label>
-        </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="id_categoria">Categoría</label>
+                                    <select name="id_categoria" id="id_categoria" class="form-control" required>
+                                        @foreach ($categorias as $categoria)
+                                            <option value="{{ $categoria->id }}">{{ $categoria->nombre_categoria }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
 
-        <div class="form-group">
-            <label for="contenido">Contenido</label>
-            <textarea name="contenido" id="contenido" class="form-control" required></textarea>
-        </div>
+                        <div class="form-group">
+                            <label for="contenido">Contenido</label>
+                            <textarea name="contenido" id="contenido" class="form-control" required></textarea>
+                        </div>
 
-        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapsePermisos" aria-expanded="false" aria-controls="collapsePermisos">
-            Asignar Permisos
-        </button>
+                        <div class="form-group form-check mt-2">
+                            <input type="checkbox" name="sin_aprobacion" id="sin_aprobacion" class="form-check-input">
+                            <label for="sin_aprobacion" class="form-check-label">No requiere aprobación</label>
+                        </div>
 
-        <div class="collapse" id="collapsePermisos">
-            <div class="form-group">
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Usuario (Correo)</th>
-                            <th>Leer</th>
-                            <th>Escribir</th>
-                            <th>Aprobar</th>
-                            <th>Eliminar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($usuarios as $usuario)
-                            <tr>
-                                <td>{{ $usuario->email }}</td>
-                                <td><input type="checkbox" name="permisos[{{ $usuario->id }}][puede_leer]" ></td>
-                                <td><input type="checkbox" name="permisos[{{ $usuario->id }}][puede_escribir]"></td>
-                                <td><input type="checkbox" name="permisos[{{ $usuario->id }}][puede_aprobar]"></td>
-                                <td><input type="checkbox" name="permisos[{{ $usuario->id }}][puede_eliminar]"></td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        <button class="btn btn-primary" type="button" data-toggle="collapse"
+                            data-target="#collapsePermisos" aria-expanded="false" aria-controls="collapsePermisos">
+                            Asignar Permisos
+                        </button>
+
+                        <div class="collapse" id="collapsePermisos">
+                            <div class="form-group">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>Usuario (Correo)</th>
+                                            <th>Leer</th>
+                                            <th>Escribir</th>
+                                            <th>Aprobar</th>
+                                            <th>Eliminar</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($usuarios as $usuario)
+                                            <tr>
+                                                <td>{{ $usuario->email }}</td>
+                                                <td><input type="checkbox" name="permisos[{{ $usuario->id }}][puede_leer]">
+                                                </td>
+                                                <td><input type="checkbox"
+                                                        name="permisos[{{ $usuario->id }}][puede_escribir]"></td>
+                                                <td><input type="checkbox"
+                                                        name="permisos[{{ $usuario->id }}][puede_aprobar]"></td>
+                                                <td><input type="checkbox"
+                                                        name="permisos[{{ $usuario->id }}][puede_eliminar]"></td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                        <button type="button" class="btn btn-secondary" onclick="confirmAndRedirect();">Volver</button>
+
+                    </form>
+                </div>
             </div>
         </div>
-
-        <button type="submit" class="btn btn-primary">Guardar</button>
-        <button type="button" class="btn btn-secondary" onclick="confirmAndRedirect();">Volver</button>
-
-    </form>
-</div>
+    </div>
 @endsection
 
 @section('scripting')
-<script>
-    function confirmAndRedirect() {
-        Swal.fire({
-            title: 'Volver sin guardar',
-            text: 'Los datos cargados se perderán ¿Estás seguro de que deseas volver?',
-            showCancelButton: true,
-            confirmButtonText: 'Sí, volver',
-            cancelButtonText: 'Cancelar',
-            customClass: {
-            confirmButton: 'btn btn-warning', // Cambia 'btn btn-danger' al color que desees
-            cancelButton: 'btn btn-primary' // Cambia 'btn btn-secondary' al color que desees
-        },
-        buttonsStyling: false
-    }).then((result) => {
-            if (result.isConfirmed) {
-                window.location.href = "{{ route('documentos.index') }}";
-            }
-        });
-    }
-</script>
+    <script>
+        function confirmAndRedirect() {
+            Swal.fire({
+                title: 'Volver sin guardar',
+                text: 'Los datos cargados se perderán ¿Estás seguro de que deseas volver?',
+                showCancelButton: true,
+                confirmButtonText: 'Sí, volver',
+                cancelButtonText: 'Cancelar',
+                customClass: {
+                    confirmButton: 'btn btn-warning', // Cambia 'btn btn-danger' al color que desees
+                    cancelButton: 'btn btn-primary' // Cambia 'btn btn-secondary' al color que desees
+                },
+                buttonsStyling: false
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "{{ route('documentos.index') }}";
+                }
+            });
+        }
+    </script>
 @endsection
--- resources/views/documentos/edit.blade.php
+++ resources/views/documentos/edit.blade.php
@@ -5,111 +5,139 @@
 @endsection
 
 @section('contenidoPrincipal')
-<div class="container" style="margin-top: 80px;">
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
+    <div class="container" style="margin-top: 80px;">
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        <div class="w-100" style="background-color: #f8f9fa;">
+            <h2 class="text-center">Editar Documento</h2>
         </div>
-    @endif
+        <div class="container" style="margin-top: 80px;">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <form action="{{ route('documentos.update', $documento->id) }}" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
 
-    <div class="w-100" style="background-color: #f8f9fa;">
-        <h2 class="text-center">Editar Documento</h2>
-    </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="titulo">Título</label>
+                                    <input type="text" name="titulo" id="titulo" class="form-control"
+                                        value="{{ $documento->titulo }}" required>
+                                </div>
+                            </div>
 
-    <form action="{{ route('documentos.update', $documento->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="id_categoria">Categoría</label>
+                                    <select name="id_categoria" id="id_categoria" class="form-control" required>
+                                        @foreach ($categorias as $categoria)
+                                            <option value="{{ $categoria->id }}"
+                                                {{ $documento->id_categoria == $categoria->id ? 'selected' : '' }}>
+                                                {{ $categoria->nombre_categoria }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
 
-        <div class="form-group">
-            <label for="titulo">Título</label>
-            <input type="text" name="titulo" id="titulo" class="form-control" value="{{ $documento->titulo }}" required>
-        </div>
+                        <div class="form-group form-check mt-2">
+                            <input type="checkbox" name="sin_aprobacion" id="sin_aprobacion" class="form-check-input"
+                                {{ $documento->estado === 'registro' ? 'checked' : '' }}>
+                            <label for="sin_aprobacion" class="form-check-label">No requiere aprobación</label>
+                        </div>
 
-        <div class="form-group">
-            <label for="id_categoria">Categoría</label>
-            <select name="id_categoria" id="id_categoria" class="form-control" required>
-                @foreach($categorias as $categoria)
-                    <option value="{{ $categoria->id }}" {{ $documento->id_categoria == $categoria->id ? 'selected' : '' }}>
-                        {{ $categoria->nombre_categoria }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
+                        <button class="btn btn-primary" type="button" data-toggle="collapse"
+                            data-target="#collapsePermisos" aria-expanded="false" aria-controls="collapsePermisos">
+                            <i class="fa-solid fa-user-shield"></i> Asignar Permisos
+                        </button>
 
-        <div class="form-group form-check mt-2">
-            <input type="checkbox" name="sin_aprobacion" id="sin_aprobacion" class="form-check-input"
-                    {{ $documento->estado === 'registro' ? 'checked' : '' }}>
-            <label for="sin_aprobacion" class="form-check-label">No requiere aprobación</label>
-        </div>
+                        <div class="collapse" id="collapsePermisos">
+                            <div class="form-group">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>Usuario (Correo)</th>
+                                            <th>Leer</th>
+                                            <th>Escribir</th>
+                                            <th>Aprobar</th>
+                                            <th>Eliminar</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($usuarios as $usuario)
+                                            @php
+                                                $permisoActual = $documento->permisos
+                                                    ->where('user_id', $usuario->id)
+                                                    ->first();
+                                            @endphp
+                                            <tr>
+                                                <td>{{ $usuario->email }}</td>
+                                                <td><input type="checkbox" name="permisos[{{ $usuario->id }}][puede_leer]"
+                                                        {{ $permisoActual && $permisoActual->puede_leer ? 'checked' : '' }}>
+                                                </td>
+                                                <td><input type="checkbox"
+                                                        name="permisos[{{ $usuario->id }}][puede_escribir]"
+                                                        {{ $permisoActual && $permisoActual->puede_escribir ? 'checked' : '' }}>
+                                                </td>
+                                                <td><input type="checkbox"
+                                                        name="permisos[{{ $usuario->id }}][puede_aprobar]"
+                                                        {{ $permisoActual && $permisoActual->puede_aprobar ? 'checked' : '' }}>
+                                                </td>
+                                                <td><input type="checkbox"
+                                                        name="permisos[{{ $usuario->id }}][puede_eliminar]"
+                                                        {{ $permisoActual && $permisoActual->puede_eliminar ? 'checked' : '' }}>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
 
-        <button class="btn btn-primary" type="button" data-toggle="collapse" 
-                data-target="#collapsePermisos" aria-expanded="false" aria-controls="collapsePermisos">
-            <i class="fa-solid fa-user-shield"></i> Asignar Permisos
-        </button>
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa-solid fa-floppy-disk"></i> Guardar Cambios
+                        </button>
+                        <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="top"
+                            title="Ver"
+                            onclick="window.location.href='{{ route('documentos.validaPermiso', ['id' => $documento, 'ruta' => 'documentos.show', 'permiso' => 'puedeLeer']) }}'">
+                            <i class="fa-solid fa-eye"></i> Detalle
+                        </button>
+                        <button type="button" class="btn btn-secondary" onclick="confirmAndRedirect();">
+                            <i class="fa-solid fa-arrow-left"></i> Volver
+                        </button>
 
-        <div class="collapse" id="collapsePermisos">
-            <div class="form-group">
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Usuario (Correo)</th>
-                            <th>Leer</th>
-                            <th>Escribir</th>
-                            <th>Aprobar</th>
-                            <th>Eliminar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($usuarios as $usuario)
-                            @php
-                                $permisoActual = $documento->permisos->where('user_id', $usuario->id)->first();
-                            @endphp
-                            <tr>
-                                <td>{{ $usuario->email }}</td>
-                                <td><input type="checkbox" name="permisos[{{ $usuario->id }}][puede_leer]" {{ $permisoActual && $permisoActual->puede_leer ? 'checked' : '' }}></td>
-                                <td><input type="checkbox" name="permisos[{{ $usuario->id }}][puede_escribir]" {{ $permisoActual && $permisoActual->puede_escribir ? 'checked' : '' }}></td>
-                                <td><input type="checkbox" name="permisos[{{ $usuario->id }}][puede_aprobar]" {{ $permisoActual && $permisoActual->puede_aprobar ? 'checked' : '' }}></td>
-                                <td><input type="checkbox" name="permisos[{{ $usuario->id }}][puede_eliminar]" {{ $permisoActual && $permisoActual->puede_eliminar ? 'checked' : '' }}></td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                    </form>
+                </div>
             </div>
         </div>
-
-        <button type="submit" class="btn btn-primary">
-            <i class="fa-solid fa-floppy-disk"></i> Guardar Cambios
-        </button>
-        <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Ver"
-                onclick="window.location.href='{{ route('documentos.validaPermiso', ['id' => $documento, 'ruta' => 'documentos.show', 'permiso' => 'puedeLeer']) }}'">
-            <i class="fa-solid fa-eye"></i> Detalle
-        </button>
-        <button type="button" class="btn btn-secondary" onclick="confirmAndRedirect();">
-            <i class="fa-solid fa-arrow-left"></i> Volver
-        </button>
-
-    </form>
-</div>
+    </div>
 @endsection
 @section('scripting')
-<script>
-    function confirmAndRedirect() {
-        Swal.fire({
-            title: 'Volver sin guardar',
-            text: 'Los datos cargados se perderán ¿Estás seguro de que deseas volver?',
-            showCancelButton: true,
-            confirmButtonText: 'Sí, volver',
-            cancelButtonText: 'Cancelar',
-            customClass: {
-            confirmButton: 'btn btn-warning', // Cambia 'btn btn-danger' al color que desees
-            cancelButton: 'btn btn-primary' // Cambia 'btn btn-secondary' al color que desees
-        },
-        buttonsStyling: false         
-        }).then((result) => {
-            if (result.isConfirmed) {
-                window.location.href = "{{ route('documentos.index') }}";
-            }
-        });
-    }
-</script>
+    <script>
+        function confirmAndRedirect() {
+            Swal.fire({
+                title: 'Volver sin guardar',
+                text: 'Los datos cargados se perderán ¿Estás seguro de que deseas volver?',
+                showCancelButton: true,
+                confirmButtonText: 'Sí, volver',
+                cancelButtonText: 'Cancelar',
+                customClass: {
+                    confirmButton: 'btn btn-warning', // Cambia 'btn btn-danger' al color que desees
+                    cancelButton: 'btn btn-primary' // Cambia 'btn btn-secondary' al color que desees
+                },
+                buttonsStyling: false
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "{{ route('documentos.index') }}";
+                }
+            });
+        }
+    </script>
 @endsection
